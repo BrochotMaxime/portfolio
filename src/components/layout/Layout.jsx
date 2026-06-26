5633eb7dd6c87db5ec8bea1d14a26c55a20514a3
BrochotMaxime <brochot.max@gmail.com>
2026-06-26 14:10:34 +0200

    feat: create application layout

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Portfolio</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
