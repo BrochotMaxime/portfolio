afc4671518b5efd224bd64f6cbd7213cdf2bc8fb
BrochotMaxime <brochot.max@gmail.com>
2026-06-27 16:48:01 +0200

    feat: set up application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
 import Layout from "./components/layout/Layout";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import LegalNoticePage from "./pages/LegalNoticePage";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="projects" element={<ProjectsPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="legal-notice" element={<LegalNoticePage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
-import Header from "./Header";
+import { Outlet } from "react-router-dom";
+
 import Footer from "./Footer";
+import Header from "./Header";
 
 function Layout() {
   return (
     <>
       <Header />
       <main>
-        <h1>Portfolio</h1>
+        <Outlet />
       </main>
       <Footer />
     </>
