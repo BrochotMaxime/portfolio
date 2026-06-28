62616b0f7965fd1062cbcf5718e10d4cdb3abcd3
BrochotMaxime <brochot.max@gmail.com>
2026-06-28 17:40:14 +0200

    feat: create UI base components

---
+++ src/components/ui/SectionTitle.jsx
@@ -0,0 +1,13 @@
+function SectionTitle({ title, subtitle }) {
+  return (
+    <header className="section-title">
+      <h2 className="section-title__heading">{title}</h2>
+
+      {subtitle && (
+        <p className="section-title__subtitle">{subtitle}</p>
+      )}
+    </header>
+  );
+}
+
+export default SectionTitle;
